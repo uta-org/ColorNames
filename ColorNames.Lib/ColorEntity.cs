ce60545b8a3a91a57b07e4e3bfb6c9211bd7c32d
Ikillnukes <ikillnukes1@gmail.com>
2019-06-27 18:57:35 +0200

    Removed unused packages and references

--- ColorNames.Lib/ColorEntity.cs
+++ ColorNames.Lib/ColorEntity.cs
@@ -1,15 +1,9 @@
-﻿using CsvHelper.Configuration.Attributes;
-
-namespace ColorNames.Lib
+﻿namespace ColorNames.Lib
 {
     public class ColorEntity
     {
-        // [Name("name")]
-        [Index(0)]
         public string Name { get; set; }
 
-        // [Name("hex")]
-        [Index(1)]
         public string Hex { get; set; }
 
         public override string ToString()
